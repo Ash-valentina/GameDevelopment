cc26ceccebee20cce994ed28678c89702840dd31
ash masala <annavalentina2006.am@gmail.com>
2024-03-04 10:33:02 +0100

    opdracht01

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class basicProgram
+{
+    static void Main()
+    {
+        int muis, sum, nummer;
+        muis = 1;
+        sum = muis * 5;
+        Console.WriteLine("Mijn nieuwe E-sports ready compatible for PC muis met rgb belichting, 1,4 M lange kabel.");
+        Console.WriteLine("hoe duur is ie? " + sum);
+        
+        Console.WriteLine("hoe duur dacht je dat de muis");
+        nummer = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("weet je zeker dat je dat denkt, dat je denkt dat de muis " + nummer + " euro kost?");
+
+        if(nummer != 5)
+        {
+            Console.WriteLine("jij bent niet rgb belichte muis pc compatible waardig");
+        }
+        else
+        {
+            Console.WriteLine("Je hebt het helemaal goed");
+        }
+
+
+    }
+}
